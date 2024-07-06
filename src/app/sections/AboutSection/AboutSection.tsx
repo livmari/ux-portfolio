3ae3db004de676bac4e579749c2d09d2664b7de1
Liv Mari <liv.alterskjaer@icloud.com>
2024-07-06 20:19:17 +0200

    add heading styles

--- src/app/sections/AboutSection/AboutSection.tsx
+++ src/app/sections/AboutSection/AboutSection.tsx
@@ -1,7 +1,7 @@
 const AboutSection: React.FC = () => {
   return (
     <section>
-      <h2>About me</h2>
+      <h2 className={'heading-2'}>About me</h2>
     </section>
   )
 }
--- src/app/sections/ContactSection/ContactSection.tsx
+++ src/app/sections/ContactSection/ContactSection.tsx
@@ -1,6 +1,6 @@
 const ContactSection: React.FC = () => (
   <section>
-    <h2>Contact me</h2>
+    <h2 className={'heading-2'}>Contact me</h2>
   </section>
 )
 
