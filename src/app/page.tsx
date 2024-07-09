ed5466ddc36f347da86ca36dec8c149fe5eac635
Liv Mari <liv.alterskjaer@icloud.com>
2024-07-09 21:27:57 +0200

    add small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
         <h1 className={`heading-1`}>
           Hi, I'm Liv Mari
           <br />
-          I design bespoke digital experiences
+          I design thoughtful digital experiences
           <br />
           that spark joy and empower users
         </h1>
