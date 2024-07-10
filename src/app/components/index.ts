f9bb7dbb07840e7b03b448634ab617f41fa932d7
Liv Mari <liv.alterskjaer@icloud.com>
2024-07-10 06:35:25 +0200

    remove old components

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,3 +1,2 @@
 export { default as Accordion } from './Accordion/Accordion'
 export { default as Card } from './Card/Card'
-export { default as ProjectCard } from './ProjectCard/ProjectCard'
--- src/app/sections/ProjectsSection/ProjectsSection.tsx
+++ src/app/sections/ProjectsSection/ProjectsSection.tsx
@@ -1,7 +1,7 @@
-import { projects } from '@/lib/projects'
-
-import { Card, ProjectCard } from '@/app/components'
 import { nanoid } from 'nanoid'
+
+import { projects } from '@/lib/projects'
+import { Card } from '@/app/components'
 
 import styles from './ProjectsSection.module.scss'
 
@@ -10,18 +10,6 @@ const ProjectsSection: React.FC = () => (
     <h2 className={'heading-2'}>Selected work</h2>
 
     <div className={styles.bento}>
-      {projects.map(project => (
-        <ProjectCard
-          image={project.bannerImage}
-          title={project.title}
-          year={project.year}
-          company={project.company}
-          key={nanoid()}
-        />
-      ))}
-    </div>
-
-    <div className={styles.projectsGrid}>
       {projects.map(project => (
         <Card
           company={project.company}
