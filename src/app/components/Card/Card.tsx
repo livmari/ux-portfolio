d558b08ab980e8e67805e5df5abbb4f95c9dea3a
Liv Mari <liv.alterskjaer@icloud.com>
2024-07-14 20:15:14 +0200

    small style changes

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import styles from './Card.module.scss'
 
 interface CardProps {
@@ -17,9 +19,35 @@ const Card: React.FC<CardProps> = ({ company, year, title }) => (
     </div>
 
     <div className={styles.previewFrame}>
-      <div className={styles.preview}>1</div>
-      <div className={styles.preview}>2</div>
-      <div className={styles.preview}>3</div>
+      <Image
+        src={
+          'https://images.unsplash.com/photo-1601972602237-8c79241e468b?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        }
+        height={400}
+        width={400}
+        alt={'Design preview 👀'}
+        className={styles.preview}
+      />
+
+      <Image
+        src={
+          'https://images.unsplash.com/photo-1512758017271-d7b84c2113f1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D0'
+        }
+        height={400}
+        width={400}
+        alt={'Design preview 👀'}
+        className={styles.preview}
+      />
+
+      <Image
+        src={
+          'https://images.unsplash.com/photo-1601972602237-8c79241e468b?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        }
+        height={400}
+        width={400}
+        alt={'Design preview 👀'}
+        className={styles.preview}
+      />
     </div>
   </button>
 )
