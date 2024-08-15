3da6dca1b3082436898f89f9009c8fd1a5506898
Liv Mari <liv.alterskjaer@icloud.com>
2024-08-15 16:43:10 +0200

    add testimonial

--- src/lib/testimonials.ts
+++ src/lib/testimonials.ts
@@ -4,6 +4,11 @@ const testimonials = [
     name: 'Desirée Roos',
     profession: 'Agile team manager @ Volvo Cars',
   },
+  {
+    quote: `As a product owner it was a delight to work with Liv Mari as our UX lead. Liv Mari is equally professional and committed as she is fun and easy to work with. She is very knowledgeable and proficient in the UX area but can also manage difficult conversations with a ranging audience without ever losing focus on the task at hand or getting thrown off balance.`,
+    name: 'Juha Kopare',
+    profession: 'Product owner @ Volvo Cars',
+  },
 ]
 
 export { testimonials }
