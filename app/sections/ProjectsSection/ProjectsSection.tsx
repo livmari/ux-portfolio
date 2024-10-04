dbfbad94857926c5dedddd32c17c6e265699f867
livmari <livmari.lervik@icloud.com>
2024-10-04 10:14:32 +0200

    move projects to separate constants file

--- app/sections/ProjectsSection/ProjectsSection.tsx
+++ app/sections/ProjectsSection/ProjectsSection.tsx
@@ -4,69 +4,10 @@ import { nanoid } from 'nanoid'
 import { useMediaQuery } from 'react-responsive'
 
 import { SCREEN_SM, SCREEN_MD, SCREEN_LG } from '@/app/constants/screens'
+import projects from '@/app/constants/projects'
 import { ProjectCard } from '@/app/components'
 
 import styles from './ProjectsSection.module.scss'
-
-const projects = [
-  {
-    image: {
-      path: 'https://images.unsplash.com/photo-1552664688-cf412ec27db2?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: '',
-    },
-    title: 'Eliminating knowledge silos by enabling cross team collaboration',
-    subtitle:
-      'Research, strategy, workshop facilitation, stakeholder management and design for Volvo Cars, 2024',
-    slug: 'eliminating-knowledge-silos-by-enabling-cross-team-collaboration',
-  },
-  {
-    image: {
-      path: 'https://images.unsplash.com/photo-1531403009284-440f080d1e12?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: '',
-    },
-    title:
-      'Gamifying bug resolution to create community between DevOps and users',
-    subtitle: 'UI design for Volvo Cars, 2024',
-    slug: 'gamifying-bug-resolution-to-create-community-between-devops-and-users',
-  },
-  {
-    image: {
-      path: 'https://images.unsplash.com/photo-1643576780112-d390f5de9241?q=80&w=878&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: '',
-    },
-    title:
-      'Utilising design system management to increase organisational UX maturity',
-    subtitle: 'Strategy, facilitation & UI design for Volvo Cars, 2024',
-    slug: 'utilising-design-system-management-to-increase organisational-ux-maturity',
-  },
-  {
-    image: {
-      path: 'https://plus.unsplash.com/premium_photo-1683133924436-a7afbdf8cd25?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: '',
-    },
-    title: 'Intuitive navigation & search in a jungle of information',
-    subtitle: 'Research and design for Volvo Cars, 2023-2024',
-    slug: 'intuitive-navigation-&-search-in-a-jungle-of-information',
-  },
-  {
-    image: {
-      path: 'https://plus.unsplash.com/premium_photo-1661382011487-cd3d6b1d9dff?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: '',
-    },
-    title: `Enabling informed freedom of choice in women's health`,
-    subtitle: 'Research and design, passion project',
-    slug: 'enabling-informed-freedom-of-choice-in-womens-health',
-  },
-  {
-    image: {
-      path: 'https://images.unsplash.com/photo-1680016661694-1cd3faf31c3a?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: '',
-    },
-    title: 'Teaching financial litteracy in a "buy now - pay later" world',
-    subtitle: 'Research and design, passion project',
-    slug: 'teaching-financial-litteracy-in-a-buy-now-pay-later-world',
-  },
-]
 
 const ProjectsSection: React.FC = () => {
   const isMobile = useMediaQuery({ maxWidth: SCREEN_SM - 1 })
